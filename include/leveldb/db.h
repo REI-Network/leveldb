1162b5702d2b82e1a6f9b5597c4e8262b7d893b4
samlior <samlior@foxmail.com>
2022-06-24 13:48:32 +0800

    Change version to 1.25.

--- include/leveldb/db.h
+++ include/leveldb/db.h
@@ -16,7 +16,7 @@ namespace leveldb {
 
 // Update CMakeLists.txt if you change these
 static const int kMajorVersion = 1;
-static const int kMinorVersion = 24;
+static const int kMinorVersion = 25;
 
 struct Options;
 struct ReadOptions;
